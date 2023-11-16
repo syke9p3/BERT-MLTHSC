634ffe2635ea72bd53def9a10ee3983d7084e92e
Ria Mabansag <riakbmabansag@gmail.com>
2023-11-16 19:42:09 +0800

    frontend: add word count

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ const inputText = document.getElementById("input-text");
 const analyzeBtn = document.getElementById("analyze-btn");
 const clearBtn = document.getElementById("clear-btn");
 const labelsContainer = document.getElementById("labels-container");
+const wordCountElement = document.getElementById('word-count');
 // let savedPosts = [] // TODO: save Posts in a database and fetch from there
 
 
@@ -9,52 +10,50 @@ analyzeBtn.addEventListener("click", () => {
 
     // 1. Send the input to the server (make a get request with url parameters)
     // 2. fetch then display the results
-
-    console.log("Input Text: " + inputText.value)
-    fetchLabels()
-
+    
+    console.log("Input Text: " + inputText.value);
+    fetchLabels();
 });
 
 clearBtn.addEventListener("click", () => {
-    inputText.value = ""
-    labelsContainer.innerHTML =
-        `
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Age
-                        </div>
-                    </div>
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Gender
-                        </div>
-                    </div>
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Physical
-                        </div>
-                    </div>
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Race
-                        </div>
-                    </div>
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Religion
-                        </div>
-                    </div>
-                    <div class="label-container">
-                        <div class="label border-none">
-                            <span class="label-percent">0.00%</span>&nbsp;&nbsp;Others
-                        </div>
-                    </div>
-        `
-})
-
+    inputText.value = "";
+    labelsContainer.innerHTML = `
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Age
+            </div>
+        </div>
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Gender
+            </div>
+        </div>
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Physical
+            </div>
+        </div>
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Race
+            </div>
+        </div>
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Religion
+            </div>
+        </div>
+        <div class="label-container">
+            <div class="label border-none">
+                <span class="label-percent">0.00%</span>&nbsp;&nbsp;Others
+            </div>
+        </div>
+    `;
+    
+    updateWordCount();
+});
 
 function fetchLabels() {
-
     fetch(`http://127.0.0.1:5000/labels?input=${inputText.value}`)
         .then(response => response.json())
         .then(data => {
@@ -66,12 +65,12 @@ function fetchLabels() {
             for (let label of labels) {
                 const probability = parseFloat(label.probability); // Convert string to float
 
-                resultHTML +=
-                    `<div class="label-container">
-                    <div class="label label-${label.name} border-none" style="width: ${probability}%;">
-                    <span class="label-percent label-percent-${label.name}">${label.probability}</span>&nbsp;&nbsp;${label.name}
-                    </div>
-                </div>`;
+                resultHTML += `
+                    <div class="label-container">
+                        <div class="label label-${label.name} border-none" style="width: ${probability}%;">
+                            <span class="label-percent label-percent-${label.name}">${label.probability}</span>&nbsp;&nbsp;${label.name}
+                        </div>
+                    </div>`;
             }
 
             labelsContainer.innerHTML = resultHTML;
@@ -79,4 +78,23 @@ function fetchLabels() {
         .catch(error => {
             console.error('Error:', error);
         });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputElement = document.getElementById('input-text');
+
+    inputElement.addEventListener('input', updateWordCount);
+
+    updateWordCount();
+});
+
+function updateWordCount() {
+    const wordCount = inputText.value.trim().split(/\s+/).filter(Boolean).length;
+    wordCountElement.textContent = wordCount;
+
+    if (wordCount < 3) {
+        wordCountElement.style.color = 'red';
+    } else {
+        wordCountElement.style.color = 'black';
+    }
 }

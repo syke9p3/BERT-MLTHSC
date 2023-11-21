fd6629f36781b4cdfcd16a857342f2e21172d7f2
Denise Gozum <69788360+Xenoxianne@users.noreply.github.com>
2023-11-21 23:43:12 +0800

    Improved label display
    - added button to show other labels lower than 50%
    - added catch if all labels are lower than 50%

--- frontend/script.js
+++ frontend/script.js
@@ -3,17 +3,93 @@ const analyzeBtn = document.getElementById("analyze-btn");
 const clearBtn = document.getElementById("clear-btn");
 const labelsContainer = document.getElementById("labels-container");
 const wordCountElement = document.getElementById("word-count");
+const toggleLabelsBtn = document.getElementById("toggle-labels-btn");
+const hideLabelsContainer = document.getElementById("hide-labels-container");
+const noLabelsContainer = document.getElementById("no-labels-container");
 
 analyzeBtn.addEventListener("click", () => {
   console.log("Input Text: " + inputText.value);
-  fetchLabels();
+  showAnalyzingState();
+  setTimeout(() => {
+    fetchLabels();
+  }, 3000);
 });
+
+function showAnalyzingState() {
+  labelsContainer.classList.add("fade-out");
+  hideLabelsContainer.style.display = "none";
+
+  setTimeout(() => {
+    labelsContainer.innerHTML = `
+      <div class="analyze_container">
+          <p>Analyzing</p>
+          <span class="loading-spinner"></span>
+      </div>
+  `;
+    labelsContainer.classList.remove("fade-out");
+  }, 500);
+
+  // Scroll down to the labels container
+  labelsContainer.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+toggleLabelsBtn.addEventListener("click", () => {
+  const labelElements = document.querySelectorAll(".label-container");
+
+  labelElements.forEach((label) => {
+    const labelPercent = label.querySelector(".label-percent");
+    const percentValue = parseFloat(labelPercent.textContent);
+
+    if (percentValue < 50 || label.style.display === "none") {
+      label.style.display = label.style.display === "none" ? "block" : "none";
+    }
+  });
+
+  noLabelsContainer.style.display =
+    noLabelsContainer.style.display === "none" ? "block" : "none";
+
+  const buttonText = toggleLabelsBtn.innerHTML.trim();
+  toggleLabelsBtn.innerHTML =
+    buttonText === 'Show Labels below 50% <i class="bx bx-chevron-down"></i>'
+      ? 'Hide Labels below 50% <i class="bx bx-chevron-up"></i>'
+      : 'Show Labels below 50% <i class="bx bx-chevron-down"></i>';
+});
+
+function hideLabelsInitially() {
+  const labelElements = document.querySelectorAll(".label-container");
+  let allLabelsBelow50 = true;
+
+  labelElements.forEach((label) => {
+    const labelPercent = label.querySelector(".label-percent");
+    const percentValue = parseFloat(labelPercent.textContent);
+
+    label.style.display = percentValue < 50 ? "none" : "block";
+    hideLabelsContainer.style.display = "block";
+
+    if (percentValue >= 50) {
+      allLabelsBelow50 = false;
+    }
+  });
+
+  if (allLabelsBelow50) {
+    labelElements.forEach((label) => {
+      label.style.display = "none";
+    });
+
+    noLabelsContainer.style.display = "block";
+  }
+}
 
 clearBtn.addEventListener("click", () => {
   inputText.value = "";
   document.getElementById("sample-hate-speech").selectedIndex = 0;
   resetLabels();
   updateWordCount();
+  hideLabelsContainer.style.display = "none";
+  noLabelsContainer.style.display = "none";
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -34,17 +110,18 @@ function fetchLabels() {
       let resultHTML = "";
 
       for (let label of labels) {
-        const probability = parseFloat(label.probability); // Convert string to float
+        const probability = parseFloat(label.probability);
 
         resultHTML += `
-                    <div class="label-container">
-                        <div class="label label-${label.name} border-none" style="width: ${probability}%;">
-                            <span class="label-percent label-percent-${label.name}">${label.probability}</span>&nbsp;&nbsp;${label.name}
-                        </div>
-                    </div>`;
+          <div class="label-container result-fade-in">
+            <div class="label label-${label.name} border-none" style="width: ${probability}%;">
+              <span class="label-percent label-percent-${label.name}">${label.probability}</span>&nbsp;&nbsp;${label.name}
+            </div>
+          </div>`;
       }
 
       labelsContainer.innerHTML = resultHTML;
+      hideLabelsInitially();
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -109,19 +186,20 @@ function updateWordCount() {
 }
 
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle("dark-mode");
 }
 
 function toggleDarkMode() {
-  const isDarkMode = document.body.classList.toggle('dark-mode');
+  const isDarkMode = document.body.classList.toggle("dark-mode");
 
-  const selectedOption = document.getElementById("sample-hate-speech").options.selectedIndex;
-  
+  const selectedOption =
+    document.getElementById("sample-hate-speech").options.selectedIndex;
+
   const textarea = document.getElementById("input-text");
 
-  textarea.style.color = isDarkMode ? 'white' : 'black';
+  textarea.style.color = isDarkMode ? "white" : "black";
 
-  document.getElementById("sample-hate-speech").options[selectedOption].style.backgroundColor = isDarkMode ? '#333' : 'white';
+  document.getElementById("sample-hate-speech").options[
+    selectedOption
+  ].style.backgroundColor = isDarkMode ? "#333" : "white";
 }
-
-document.getElementById("toggle-dark-mode-btn").addEventListener("click", toggleDarkMode);
